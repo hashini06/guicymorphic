c28a69acc4f59096b3a2d71afc0c4d3e6f00596e
alen_vrecko <alen_vrecko@yahoo.com>
2010-07-29 08:27:14 +0000

    Fixed bug where Exception contructor does not call super constructor.

--- fw/src/guicymorphic/fw/util/PackageScanFailedException.java
+++ fw/src/guicymorphic/fw/util/PackageScanFailedException.java
@@ -3,13 +3,12 @@ package guicymorphic.fw.util;
 import java.io.IOException;
 
 /**
- * Created by IntelliJ IDEA.
- * User: alen
- * Date: Feb 27, 2010
- * Time: 7:15:49 PM
- * To change this template use File | Settings | File Templates.
+ * Used when package scan fails.
+ *
+ * @author Alen Vrecko
  */
 public class PackageScanFailedException extends RuntimeException {
     public PackageScanFailedException(String s, IOException e) {
+        super(s,e);
     }
 }
