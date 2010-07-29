5eed1619b54d96419e08410c0e5dc18334bebc76
alen_vrecko@yahoo.com <alen_vrecko@yahoo.com@622731b6-2a24-11df-a54b-535fbf054105>
2010-07-29 08:27:14 +0000

    Fixed bug where Exception contructor does not call super constructor.

--- trunk/fw/src/guicymorphic/fw/util/PropertiesNotFound.java
+++ trunk/fw/src/guicymorphic/fw/util/PropertiesNotFound.java
@@ -1,13 +1,12 @@
 package guicymorphic.fw.util;
 
 /**
- * Created by IntelliJ IDEA.
- * User: alen
- * Date: Feb 27, 2010
- * Time: 7:15:11 PM
- * To change this template use File | Settings | File Templates.
+ * Thrown when finding properties fails.
+ *
+ * @author Alen Vrecko
  */
 public class PropertiesNotFound extends RuntimeException {
     public PropertiesNotFound(String s, Exception e) {
+        super(s,e);
     }
 }
